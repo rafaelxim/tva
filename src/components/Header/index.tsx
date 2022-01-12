61d7953865a7c80d506cf51698177e69a6ea12a6
Rafael Herculano <rafa.lj@hotmail.com>
2022-01-12 09:56:28 -0300

    logout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Box from "@mui/material/Box";
+import Popper from "@mui/material/Popper";
 import Logo from "../../assets/logo_tva.svg";
 import User from "../../assets/user.jpg";
 import NotificationIcon from "../../assets/notification.svg";
@@ -9,14 +12,32 @@ import { StoreState } from "../../actions/types";
 type Props = Record<string, never>;
 
 const Header: React.FC<Props> = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const navigate = useNavigate();
   const { user } = useSelector((store: StoreState) => store.authReducer);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "logout-popper" : undefined;
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
 
   return (
     <div className="header">
       <div className="header__logo">
         <img src={Logo} alt="logo" />
       </div>
-      <div className="header__userBox">
+      <div
+        aria-describedby={id}
+        onClick={handleClick}
+        className="header__userBox"
+      >
         <div className="header__notifications">
           <img src={NotificationIcon} alt="notification" />
         </div>
@@ -28,6 +49,15 @@ const Header: React.FC<Props> = () => {
           <img src={User} alt="userPic" />
         </div>
       </div>
+      <Popper
+        placement="bottom-end"
+        onClick={() => handleLogout()}
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+      >
+        <Box sx={{ border: 1, p: 1, bgcolor: "background.paper" }}>LOGOUT</Box>
+      </Popper>
     </div>
   );
 };

7455bed8d71dcc3759e917e919bbe160df854edb
Rafael Herculano <rafa.lj@hotmail.com>
2022-03-03 09:40:38 -0300

    reset de password

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -11,17 +11,24 @@ import { StoreState } from "../../actions/types";
 import Backdrop from "../../components/Backdrop";
 import FormRow from "../../components/FormRow";
 import FormField from "../../components/FormField";
+import api from "../../services/api";
 
 const Login: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [newPassword2, setNewPassword2] = useState("");
+  const [passForgotView, setPassForgotView] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const { successLogin, loading, loginAttempts } = useSelector(
     (state: StoreState) => state.authReducer
   );
+
+  const reset_token = searchParams.get("token");
 
   useEffect(() => {
     const expired = searchParams.get("msg");
@@ -46,6 +53,90 @@ const Login: React.FC = () => {
     }
   };
 
+  const submitPassRecovery = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const payload = {
+      email,
+    };
+    if (email) {
+      try {
+        await api.post("/password_reset/", { payload });
+        dispatch(
+          setSnackbarAlert({
+            message: "As instruções foram enviadas para o seu email",
+            isVisible: true,
+            severity: "success",
+          })
+        );
+      } catch (err: any) {
+        console.log(err);
+        dispatch(
+          setSnackbarAlert({
+            message: "Erro ao tentar enviar o email",
+            isVisible: true,
+            severity: "error",
+          })
+        );
+      }
+    } else {
+      dispatch(
+        setSnackbarAlert({
+          message: "Preencha o campo de email corretamente",
+          isVisible: true,
+          severity: "error",
+        })
+      );
+    }
+  };
+
+  const submitPassRedefine = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (newPassword !== newPassword2) {
+      dispatch(
+        setSnackbarAlert({
+          message: "As senhas digitadas não conferem!",
+          isVisible: true,
+          severity: "error",
+        })
+      );
+      return;
+    }
+    if (!newPassword || !newPassword2) {
+      dispatch(
+        setSnackbarAlert({
+          message: "O campo senha é obrigatório",
+          isVisible: true,
+          severity: "error",
+        })
+      );
+      return;
+    }
+
+    try {
+      await api.post("/password_reset/confirm", {
+        password: newPassword,
+        token: reset_token,
+      });
+      dispatch(
+        setSnackbarAlert({
+          message: "A senha foi alterada!",
+          isVisible: true,
+          severity: "success",
+        })
+      );
+      setSearchParams({ token: "" });
+    } catch (err: any) {
+      dispatch(
+        setSnackbarAlert({
+          message: "Houve um erro ao tentar alterar a senha",
+          isVisible: true,
+          severity: "error",
+        })
+      );
+      console.log(err);
+    }
+  };
+
   return (
     <div className="login">
       <Backdrop open={loading} />
@@ -59,45 +150,128 @@ const Login: React.FC = () => {
         />
       </div>
       <div className="login__right">
-        <form onSubmit={(e) => submitLogin(e)} className="login__formContainer">
-          <h3 className="login__title">TvaNetPlay</h3>
-          <p className="login__subtitle">Faça seu login</p>
-          <FormRow>
-            <FormField>
-              <TextField
-                autoComplete="off"
-                className="login__input"
-                id="outlined-search"
-                label="Usuário"
-                type="text"
-                fullWidth
-                size="small"
-                value={user}
-                onChange={(e) => setUser(e.target.value)}
-              />
-            </FormField>
-          </FormRow>
+        {!passForgotView && !reset_token && (
+          <form
+            onSubmit={(e) => submitLogin(e)}
+            className="login__formContainer"
+          >
+            <h3 className="login__title">TvaNetPlay</h3>
+            <p className="login__subtitle">Faça seu login</p>
+            <FormRow>
+              <FormField>
+                <TextField
+                  autoComplete="off"
+                  className="login__input"
+                  id="outlined-search"
+                  label="Usuário"
+                  type="text"
+                  fullWidth
+                  size="small"
+                  value={user}
+                  onChange={(e) => setUser(e.target.value)}
+                />
+              </FormField>
+            </FormRow>
 
-          <FormRow>
-            <FormField>
-              <TextField
-                autoComplete="off"
-                className="login__input"
-                id="outlined-password-input"
-                label="Password"
-                type="password"
-                fullWidth
-                size="small"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </FormField>
-          </FormRow>
+            <FormRow>
+              <FormField>
+                <TextField
+                  autoComplete="off"
+                  className="login__input"
+                  id="outlined-password-input"
+                  label="Password"
+                  type="password"
+                  fullWidth
+                  size="small"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </FormField>
+            </FormRow>
 
-          <Button fullWidth type="submit" variant="contained">
-            Login
-          </Button>
-        </form>
+            <Button fullWidth type="submit" variant="contained">
+              Login
+            </Button>
+            <p onClick={() => setPassForgotView(true)} className="login__p1">
+              Esqueci a minha senha
+            </p>
+          </form>
+        )}
+
+        {passForgotView && !reset_token && (
+          <form
+            onSubmit={(e) => submitPassRecovery(e)}
+            className="login__formContainer"
+          >
+            <h3 className="login__title">TvaNetPlay</h3>
+            <p className="login__subtitle">Recupere sua conta</p>
+
+            <FormRow>
+              <FormField>
+                <TextField
+                  autoComplete="off"
+                  className="login__input"
+                  id="outlined-search"
+                  label="Email"
+                  type="text"
+                  fullWidth
+                  size="small"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormField>
+            </FormRow>
+            <Button fullWidth type="submit" variant="contained">
+              Enviar
+            </Button>
+            <p onClick={() => setPassForgotView(false)} className="login__p1">
+              Retornar para login
+            </p>
+          </form>
+        )}
+
+        {reset_token && (
+          <form
+            onSubmit={(e) => submitPassRedefine(e)}
+            className="login__formContainer"
+          >
+            <h3 className="login__title">TvaNetPlay</h3>
+            <p className="login__subtitle">Digite a nova senha</p>
+            <FormRow>
+              <FormField>
+                <TextField
+                  autoComplete="off"
+                  className="login__input"
+                  id="outlined-password-input"
+                  label="Senha"
+                  type="password"
+                  fullWidth
+                  size="small"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                />
+              </FormField>
+            </FormRow>
+            <FormRow>
+              <FormField>
+                <TextField
+                  autoComplete="off"
+                  className="login__input"
+                  id="outlined-password-input"
+                  label="Repita a senha"
+                  type="password"
+                  fullWidth
+                  size="small"
+                  value={newPassword2}
+                  onChange={(e) => setNewPassword2(e.target.value)}
+                />
+              </FormField>
+            </FormRow>
+            <Button fullWidth type="submit" variant="contained">
+              Enviar
+            </Button>
+          </form>
+        )}
       </div>
     </div>
   );
